e828b33fd1da0e429f66edf74ffcf11113003b8c
Infrastruktur-mobben <infrastruktur@tv4.se>
2018-08-16 13:10:03 +0200

    Test module

--- test/absinthe_status_code_plug_test.exs
+++ test/absinthe_status_code_plug_test.exs
@@ -1,11 +1,14 @@
 defmodule AbsintheStatusCodePlugTest do
   use ExUnit.Case
-  doctest AbsintheStatusCodePlug
+  use Plug.Test
 
   describe "when body contains an error" do
     test "override status code to 400" do
-      conn = %Plug.Conn{resp_body: ~s({"errors": []})}
-      conn = AbsintheStatusCodePlug.fix_status_code(conn)
+      conn =
+        conn(:get, "/", resp_body: %{errors: []})
+        |> AbsintheStatusCodePlug.call([])
+        |> send_resp(200, ~s({"errors": []}))
+
       assert conn.status == 400
     end
   end
