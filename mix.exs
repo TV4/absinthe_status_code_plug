bcd1beb2ddaf3b0d2e839dfa2ebb3813cae64c5f
Infrastruktur-mobben <infrastruktur@tv4.se>
2018-08-16 13:17:15 +0200

    Set version 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheStatusCodePlug.MixProject do
   def project do
     [
       app: :absinthe_status_code_plug,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
